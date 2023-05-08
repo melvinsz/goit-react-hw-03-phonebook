1296260398e941139462893ae41c129427472a6a
Stanislav <Stas.zaliiski@gmail.com>
2023-05-08 09:37:47 +0200

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,47 @@
-export const App = () => {
-  return <div></div>;
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Notiflix from 'notiflix';
+import Form from './Form/Form';
+import Contacts from './Contacts/Contacts';
+import Section from './Section/Section';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+  onGetDataForm = data => {
+    const hasName = this.state.contacts.some(
+      contact => contact.name === data.name
+    );
+    if (hasName) {
+      Notiflix.warning(`Contact "${data.name}" is already exist.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { ...data, id: nanoid() }],
+    }));
+  };
+
+  deleteItem = deletedId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== deletedId),
+    }));
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        <Section title="Phonebook">
+          <Form onSubmit={this.onGetDataForm} />
+        </Section>
+        <Section title="Contacts">
+          <Contacts contacts={contacts} onClickDelete={this.deleteItem} />
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
